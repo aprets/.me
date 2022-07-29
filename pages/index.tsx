95b213730470e68ebcb9e9d780483876903e816e
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-07-29 20:37:05 +0100

    refactor: better text spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,10 +67,10 @@ export default function Home() {
 			<div className='flex flex-col-reverse md:flex-row justify-between mb-24'>
 				<div className='flex flex-col'>
 					<h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white'>Artur Prets</h1>
-					<h2 className='text-gray-700 dark:text-gray-200 mb-4'>Human being currently at {' '}
-						<span className='font-semibold'>Home</span>
+					<h2 className='text-gray-700 dark:text-gray-200 mb-4'>
+						Human being currently at {' '} <span className='font-semibold'>Home</span>
 					</h2>
-					<p className='text-gray-600 dark:text-gray-400 mb-16'>Help I am under the water! Help I am under the water! Help I am under the water! Help I am under the water! </p>
+					<p className='text-gray-600 dark:text-gray-400 mb-4'>Help I am under the water! Help I am under the water! Help I am under the water! Help I am under the water! </p>
 				</div>
 				<div className='flex justify-center'>
 					<img
