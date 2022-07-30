f6ecf1d77542e756d19c068e3d8f205430c14956
Arthur <you@example.com>
2022-07-30 23:32:03 +0100

    feat: add platform used and benchmark link to nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link'
 import {useRouter} from 'next/router'
 
+const stringSnakeCaseToTitleCase = (input: string) => input.replace(
+	/\w\S*/g,
+	(txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(),
+)
+
 interface NavbarProps {
 	links: {url: string; label: string}[];
 }
@@ -23,10 +28,15 @@ export default function Navbar({links}: NavbarProps) {
 	})
 
 	return (
-		<nav className='mt-5 mb-10'>
+		<nav className='mt-5 mb-10 flex flex-row justify-between'>
 			<div className='flex flex-row items-center flex-wrap justify-start gap-4'>
 				{items}
 			</div>
+			<Link href='/benchmark'>
+				<a href='/benchmark' className='text-inherit no-underline'>
+					Loaded from {stringSnakeCaseToTitleCase(process.env.NEXT_PUBLIC_HOST ?? 'DEV')}
+				</a>
+			</Link>
 		</nav>
 	)
 }
