e371f4e8cbeb41c4365a91b526da45ebee45debe
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-08-01 16:41:14 +0100

    feat: add POC readout of vitals

--- pages/benchmark.tsx
+++ pages/benchmark.tsx
@@ -1,12 +1,16 @@
 import {Button, Text, Container, Group, Center, Loader, Paper, Badge, Title} from '@mantine/core'
+import {useWebVitals} from 'lib/hostBenchmark'
 
 import Link from 'next/link'
+import {useEffect} from 'react'
 
 export default function Benchmark() {
+	const vitals = useWebVitals()
 	return (
 		<Text>
 			This website is hosted on both Vercel and Cloudflare Pages to compare the performance of the two. <br />
-			Statistics of page load times are stored to compare averages between the two platforms.
+			Statistics of page load times are stored to compare averages between the two platforms.<br />
+			Your current stats: {JSON.stringify(vitals)}
 		</Text>
 
 	)
