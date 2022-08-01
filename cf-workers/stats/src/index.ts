3bb32b7ae652c4e3be571239494e2a80dcdc9718
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-08-01 17:44:52 +0100

    feat: cf worker to query average pageload stats

---
+++ cf-workers/stats/src/index.ts
@@ -0,0 +1,44 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	STATS: KVNamespace,
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+}
+
+const validHosts = ['CLOUDFLARE_PAGES', 'VERCEL', 'DEV_SERVER'] as const
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
+		if (request.method !== 'GET') {
+			return new Response('Method not allowed', {status: 405})
+		}
+		const dataPromises = validHosts.map((host) => env.STATS.get<{num: number, ttfb: number, fcp: number} | null>(host, 'json'))
+		const data = await Promise.all(dataPromises)
+		const response = data.reduce((acc, hostData, index) => ({
+			...acc,
+			[validHosts[index]]: {
+				ttfb: hostData ? hostData.ttfb : -1,
+				fcp: hostData ? hostData.fcp : -1,
+			},
+		}), {})
+		return new Response(JSON.stringify(response))
+	},
+}
