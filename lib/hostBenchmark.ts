92a7e340cc162aca26cd630b4ccd643d1f879bb9
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-08-01 16:40:39 +0100

    feat: add means for collecting vitals for benchmark

--- lib/hostBenchmark.ts
+++ lib/hostBenchmark.ts
@@ -1,5 +1,34 @@
+import {useEffect, useState} from 'react'
+
 export const hostTag = process.env.NEXT_PUBLIC_HOST ?? 'DEV_SERVER'
 
 const snakeCaseToTitleCase = (input: string) => input.toLowerCase().replace(/^_*(.)|_+(.)/g, (s, c, d) => (c ? c.toUpperCase() : ` ${d.toUpperCase()}`))
 
 export const humanHostName = snakeCaseToTitleCase(hostTag)
+
+interface Metrics {
+	ttfb: number,
+	fcp: number,
+}
+
+let metrics: Metrics | undefined
+
+const metricsCallbacks: ((update: typeof metrics) => void)[] = []
+
+export const setMetrics = (update: Metrics) => {
+	console.log(update)
+	metrics = update
+	metricsCallbacks.forEach((cb) => cb(metrics))
+}
+
+export const useWebVitals = () => {
+	const [stateMetrics, setStateMetrics] = useState<Metrics>()
+	useEffect(() => {
+		setStateMetrics(metrics)
+		metricsCallbacks.push(setStateMetrics)
+		return () => {
+			metricsCallbacks.splice(metricsCallbacks.indexOf(setStateMetrics), 1)
+		}
+	}, [])
+	return stateMetrics
+}
