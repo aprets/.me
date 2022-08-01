07f31d011207966b189c484121bbbe7b759739ce
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-08-01 18:11:22 +0100

    feat: submit pageload data to loads worker

--- lib/hostBenchmark.ts
+++ lib/hostBenchmark.ts
@@ -1,5 +1,7 @@
 import {useEffect, useState} from 'react'
 
+const beaconUrl = 'https://loads.aprets.workers.dev'
+
 export const hostTag = process.env.NEXT_PUBLIC_HOST ?? 'DEV_SERVER'
 
 const snakeCaseToTitleCase = (input: string) => input.toLowerCase().replace(/^_*(.)|_+(.)/g, (s, c, d) => (c ? c.toUpperCase() : ` ${d.toUpperCase()}`))
@@ -19,6 +21,23 @@ export const setMetrics = (update: Metrics) => {
 	console.log(update)
 	metrics = update
 	metricsCallbacks.forEach((cb) => cb(metrics))
+	const beaconBody = JSON.stringify({
+		host: hostTag,
+		ttfb: metrics.ttfb,
+		fcp: metrics.fcp,
+	})
+	if (navigator.sendBeacon) {
+		navigator.sendBeacon(beaconUrl, beaconBody)
+	} else {
+		fetch(beaconUrl, {
+			body: beaconBody,
+			method: 'POST',
+			keepalive: false,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+	}
 }
 
 export const useWebVitals = () => {
