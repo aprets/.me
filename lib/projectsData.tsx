31979b7ea9611d9a02a759913cd497b51d879715
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-08-02 23:29:35 +0100

    feat: add more project data

--- lib/projectsData.tsx
+++ lib/projectsData.tsx
@@ -3,6 +3,8 @@ import {StaticImageData} from 'next/image'
 
 import vmoteImg from 'public/vmote.png'
 import clearpathImg from 'public/clearpath.png'
+import ubntImg from 'public/ubnt-cloudflared.png'
+import apretsImg from 'public/aprets.png'
 
 export const projectTags = [
 	'JavaScript',
@@ -17,11 +19,18 @@ export const projectTags = [
 	'Google Cloud',
 	'Terraform',
 	'Contentful',
+	'Tailwind CSS',
+	'Cloudflare Workers',
 	'Socket.IO',
 	'WebSocket',
 	'Mantine',
 	'HyperV',
 	'PowerShell',
+	'Shell Script',
+
+	'Linux',
+	'Reverse Engineering',
+	'Networking',
 ] as const
 
 export type ProjectTag = typeof projectTags[number]
@@ -31,7 +40,7 @@ export type Project = {
 	brief: string,
 	description: React.ReactNode,
 	image: StaticImageData,
-	githubLink: string,
+	githubLink?: string,
 	tags: ProjectTag[],
 }
 
@@ -41,7 +50,7 @@ export const projects: Project[] = [
 		brief: 'Web remote management interface for headless Windows streaming servers',
 		description: (
 			<>
-				The project allows the user to remotely control server power, active output screen and lets
+				Allows the user to remotely control server power, active output screen and lets
 				them manage HyperV Virtual Machines on the server.
 				Status and statistics are streamed to the user in real-time allowing for full remote management.
 				PowerShell and NVIDIA System Management Interface were used to collect data and interact
@@ -58,7 +67,7 @@ export const projects: Project[] = [
 		brief: 'Prototype DX-focused infrastructure management platform on top of public cloud',
 		description: (
 			<>
-				This project offers &quot;heroku-like&quot; experience for managing infrastructure with full flexibility and pricing of public cloud
+				&quot;Heroku-like&quot; experience for managing infrastructure with full flexibility and pricing of public cloud
 				(GCP for the prototype).
 				Works on top of the user&apos;s stack (GCP, Terraform Cloud) leaving them in full control of their infrastructure.
 				Multiple workspaces can be managed which can contain Virtual Machines, SQL Databases and Serverless Docker Container deployments.
@@ -70,37 +79,40 @@ export const projects: Project[] = [
 		githubLink: 'https://github.com/aprets/clearpath',
 		tags: ['JavaScript', 'TypeScript', 'React', 'Next', 'Google Cloud', 'Docker', 'Terraform', 'Firebase', 'GitHub Actions', 'Mantine'],
 	},
-	// {
-	// 	title: 'ubnt-cloudflared',
-	// 	brief: 'Install & Run Cloudflare Tunnel on Ubiquiti network gateways',
-	// 	description: (
-	// 		<>
-	// 			This project offers &quot;heroku-like&quot; experience for managing infrastructure with full flexibility and pricing of public cloud
-	// 			(GCP for the prototype).
-	// 			Works on top of the user&apos;s stack (GCP, Terraform Cloud) leaving them in full control of their infrastructure.
-	// 			Multiple workspaces can be managed which can contain Virtual Machines, SQL Databases and Serverless Docker Container deployments.
-	// 			The project offers comprehensive guidance and help to maximally simplify operations and allow easy onboarding of new hires and
-	// 			engineers with limited cloud experience.
-	// 		</>
-	// 	),
-	// 	image: '/ubnt-cloudflared.png',
-	// 	githubLink: 'https://github.com/aprets/ubnt-cloudflared',
-	// 	tags: ['JavaScript', 'TypeScript', 'React', 'Next', 'Google Cloud', 'Docker', 'Terraform', 'Firebase', 'GitHub Actions', 'Mantine'],
-	// },
-	// {
-	// 	title: 'Mantine 2',
-	// 	brief: 'A new version of Mantine',
-	// 	description: 'A React app for managing your personal finances',
-	// 	image: 'https://source.unsplash.com/random',
-	// 	githubLink: 'https://github.com/aprets/Chatty-Chef',
-	// 	tags: ['React', 'Firebase'],
-	// },
-	// {
-	// 	title: 'Mantine 3',
-	// 	brief: 'A new version of Mantine',
-	// 	description: 'A React app for managing your personal finances',
-	// 	image: 'https://source.unsplash.com/random',
-	// 	githubLink: 'https://github.com/aprets/Chatty-Chef',
-	// 	tags: ['React'],
-	// },
+	{
+		title: 'Ubiquiti Cloudflared',
+		brief: 'Install & Run Cloudflare Tunnel on Ubiquiti network gateways',
+		description: (
+			<>
+				<a className='text-inherit' href='https://https://dash.teams.cloudflare.com/'>Cloudflare Tunnel</a> allows for secure
+				remote access to a network with no extra configuration and simple remote management from Cloudflare Zero Trust Dashboard.
+				This project provides the scripts to install cloudflared (which can create a tunnel to the network) on a
+				{' '}<a href='https://store.ui.com/products/unifi-security-gateway'>USG</a>.
+				This allows for creation of a tunnel to a network without any additional hardware.
+				The scripts use the semi-reverse-engineered boot sequence configuration of EdgeOS and run the tunnel in parallel to all
+				the core functionality of the gateway.
+			</>
+		),
+		image: ubntImg,
+		githubLink: 'https://github.com/aprets/ubnt-cloudflared',
+		tags: ['Linux', 'Shell Script', 'Reverse Engineering', 'Networking'],
+	},
+	{
+		title: 'aprets.me',
+		brief: 'Personal website and Next.js static hosting benchmark',
+		description: (
+			<>
+				{/* <a className='text-inherit' href='https://https://dash.teams.cloudflare.com/'>Cloudflare Tunnel</a> allows for secure
+				remote access to a network with no extra configuration and simple remote management from Cloudflare Zero Trust Dashboard.
+				This project provides the scripts to install cloudflared (which can create a tunnel to the network) on a
+				{' '}<a href='https://store.ui.com/products/unifi-security-gateway'>USG</a>.
+				This allows for creation of a tunnel to a network without any additional hardware.
+				The scripts use the semi-reverse-engineered boot sequence configuration of EdgeOS and run the tunnel in parallel to all
+				the core functionality of the gateway. */}
+			</>
+		),
+		image: apretsImg,
+		githubLink: 'https://github.com/aprets/.me',
+		tags: ['JavaScript', 'TypeScript', 'React', 'Next', 'Tailwind CSS', 'Mantine', 'Cloudflare Workers'],
+	},
 ]
