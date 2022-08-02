2d02016d198a5f3fd682add1040b048f95affa51
Arthur <15954684+gentleseal@users.noreply.github.com>
2022-08-02 19:21:51 +0100

    feat: bring metrics back to mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,10 +32,10 @@ export default function Navbar({links}: NavbarProps) {
 			</div>
 			<div className='flex flex-col justify-center mb-4 md:mb-0'>
 				<Link href='/benchmark'>
-					<a className='text-inherit no-underline text-primary-800 text-sm' title='Click to learn more'>
+					<a className='no-underline text-primary-800 text-sm' title='Click to learn more'>
 						<span className='inline-block rounded-full bg-primary-100 pl-3'>
 							<span className='font-medium'>Loaded from {humanHostName} {' '}</span>
-							<span className='hidden md:inline text-xs'>
+							<span className='text-[0.7rem]'>
 								(TTFB: {vitals ? roundMetric(vitals.ttfb) : '000'}<span className='text-[0.65rem]'>ms</span>,
 								FCP: {vitals ? roundMetric(vitals.fcp) : '0000'}<span className='text-[0.65rem]'>ms</span>)
 								{' '}
