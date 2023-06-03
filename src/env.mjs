6cf52b4e7dcde391b4a27d2e55ba884cedd1aca0
aprets <15954684+aprets@users.noreply.github.com>
2023-06-03 21:49:01 +0100

    add obfuscated link component

--- src/env.mjs
+++ src/env.mjs
@@ -4,9 +4,11 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     CONTACT_EMAIL: z.string().email(),
+    CONTACT_EMAIL_LINK: z.string().url(),
   },
   client: {},
   runtimeEnv: {
     CONTACT_EMAIL: process.env.CONTACT_EMAIL,
+    CONTACT_EMAIL_LINK: process.env.CONTACT_EMAIL_LINK,
   },
 });
