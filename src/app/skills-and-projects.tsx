3de900f03248471f20d8bbe897b74f0fb3e897e7
aprets <15954684+aprets@users.noreply.github.com>
2023-06-04 20:31:32 +0100

    start using next image component for projects

--- src/app/skills-and-projects.tsx
+++ src/app/skills-and-projects.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -100,10 +101,11 @@ export const SkillsAndProjects = () => {
               <motion.div layout>
                 <div className="relative w-full">
                   <Zoom>
-                    <img
-                      src={project.image.src}
+                    <Image
+                      src={project.image}
                       alt=""
-                      className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                      className="w-full rounded-2xl bg-gray-100 object-cover aspect-[16/9] sm:aspect-[2/1] lg:aspect-[3/2]"
+                      placeholder="blur"
                     />
                   </Zoom>
                   <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10 pointer-events-none" />
